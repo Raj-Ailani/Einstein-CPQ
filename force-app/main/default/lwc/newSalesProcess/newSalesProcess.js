2a146ff2b777c082515456b3b260db62c22e05eb
Raj Ailani <ailaniraj1@gmail.com>
2023-04-04 14:53:29 +0530

    fixed , insert chat transcript

--- force-app/main/default/lwc/newSalesProcess/newSalesProcess.js
+++ force-app/main/default/lwc/newSalesProcess/newSalesProcess.js
@@ -1,6 +1,7 @@
 import { LightningElement,api,wire } from 'lwc';
 import {NavigationMixin} from 'lightning/navigation'
 import { CurrentPageReference } from 'lightning/navigation';
+import createChat from '@salesforce/apex/createChatTranscript.createChat'
 
 export default class NewSalesProcess extends  NavigationMixin(LightningElement) {
    
@@ -13,17 +14,23 @@ export default class NewSalesProcess extends  NavigationMixin(LightningElement) 
         }
      }
       navigateToComponent() {
-        if(this.recordId){
-            this[NavigationMixin.Navigate]({
-                type: "standard__navItemPage",
-                attributes: {
-                  apiName: 'Sales_Process'
-                },
-                state:{
-                    c__accountId:this.recordId
-                }
-              });
-        }
+        createChat({accountId:this.recordId}).then(result => {
+          if(result){
+              console.log(result);
+              if(this.recordId){
+                this[NavigationMixin.Navigate]({
+                    type: "standard__navItemPage",
+                    attributes: {
+                      apiName: 'Sales_Process'
+                    },
+                    state:{
+                        c__accountId:this.recordId
+                    }
+                  });
+            }
+          }
+      })
+
 
       }
 }
--- force-app/main/default/lwc/parentPage/parentPage.js
+++ force-app/main/default/lwc/parentPage/parentPage.js
@@ -4,14 +4,6 @@ import createChat from '@salesforce/apex/createChatTranscript.createChat'
 
 export default class ParentPage extends LightningElement {
     @api accountId;
-
-    connectedCallback() {
-        createChat({accountId:this.accountId}).then(result => {
-            if(result){
-                console.log(result);
-            }
-        })
-    }
     @wire(CurrentPageReference)
     getPageReferenceParameters(currentPageReference) {
         if (currentPageReference) {
