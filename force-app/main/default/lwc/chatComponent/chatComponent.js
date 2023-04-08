bb661356341d0c1f6600e660259ba2582e8cb683
Jeet Bhardiya <bhardiyajeet@gmail.com>
2023-04-09 00:44:08 +0530

    All dynamic message components created.

--- force-app/main/default/lwc/chatComponent/chatComponent.js
+++ force-app/main/default/lwc/chatComponent/chatComponent.js
@@ -2,11 +2,18 @@ import { LightningElement } from 'lwc';
 
 export default class ChatComponent extends LightningElement {
 
-    message = "Hello, how can I help you today?";
+    messageAssistant = "Hello, how can I help you today?";
+    messageUser = "Create a new Opportunity";
+    eventMessage = "New Opportunity has been created";
+    // userOrOther = 0;
 
     handleClick(event) {
         console.log("Button Works");
-        this.createAssistantElements(this.message);
+        this.createAssistantElements(this.messageAssistant);
+        this.createUserElements(this.messageUser,0);
+        this.createUserElements(this.messageUser,1);
+        this.createEventElements(this.eventMessage);
+        
     }
 
 
@@ -80,5 +87,89 @@ export default class ChatComponent extends LightningElement {
 
         ulElement.appendChild(liElement);
 
+    }
+
+    createUserElements(message,userOrOther){
+        const liElement = document.createElement("li");
+        
+        liElement.classList.add("slds-chat-listitem");
+        liElement.classList.add("slds-chat-listitem_outbound");
+
+        const parentDiv = document.createElement("div");
+        parentDiv.className = "slds-chat-message";
+
+        const childDiv = document.createElement("div");
+        childDiv.className = "slds-chat-message__body";
+
+        const grandChildDiv1 = document.createElement("div");
+        grandChildDiv1.classList.add("slds-chat-message__text");
+
+        if(userOrOther == 0){
+            grandChildDiv1.classList.add("slds-chat-message__text_outbound");
+        }
+        else{
+            grandChildDiv1.classList.add("slds-chat-message__text_outbound-agent");
+        }
+        
+
+        const messageSpan = document.createElement("span");
+        const messageSpanText = document.createTextNode(message);
+
+        messageSpan.appendChild(messageSpanText);
+
+        grandChildDiv1.appendChild(messageSpan);
+
+        const grandChildDiv2 = document.createElement("div");
+        grandChildDiv2.className = "slds-chat-message__meta";
+
+        const responseMetadataText = "Amber Cann • 5:23 PM";
+        const grandChildDiv2Text = document.createTextNode(responseMetadataText);
+
+        grandChildDiv2.appendChild(grandChildDiv2Text);
+
+        childDiv.appendChild(grandChildDiv1);
+        childDiv.appendChild(grandChildDiv2);
+
+        parentDiv.appendChild(childDiv);
+
+        liElement.appendChild(parentDiv);
+
+        
+        const ulElement = this.template.querySelector("ul");
+
+        ulElement.appendChild(liElement);
+
+    }
+
+    createEventElements(eventMessage){
+        const liElement = document.createElement("li");
+        
+        liElement.classList.add("slds-chat-listitem");
+        liElement.classList.add("slds-chat-listitem_event");
+
+        const parentDiv = document.createElement("div");
+        parentDiv.className = "slds-chat-event";
+
+        const childDiv = document.createElement("div");
+        childDiv.className = "slds-chat-event__body";
+
+        const pTag = document.createElement("p");
+        // const bTag = document.createElement("b");
+        // const bTagText = document.createTextNode("New Opportunity ");
+        const pTagText = document.createTextNode(eventMessage);
+
+        // bTag.appendChild(bTagText);
+        // pTag.appendChild(bTag);
+        pTag.appendChild(pTagText);
+
+        childDiv.appendChild(pTag);
+
+        parentDiv.appendChild(childDiv);
+
+        liElement.appendChild(parentDiv);
+
+        const ulElement = this.template.querySelector("ul");
+
+        ulElement.appendChild(liElement);
     }
 }
