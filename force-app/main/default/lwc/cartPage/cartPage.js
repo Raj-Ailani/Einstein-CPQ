70f55c60d4b5032c4dd963da9d91ec396ca4f7e7
Raj Ailani <ailaniraj1@gmail.com>
2023-04-14 22:21:10 +0530

    Merging Org with test branch

--- force-app/main/default/lwc/cartPage/cartPage.js
+++ force-app/main/default/lwc/cartPage/cartPage.js
@@ -1,6 +1,19 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import addSelectedProducts from '@salesforce/apex/addProduct.addSelectedProducts';
 
 export default class CartPage extends LightningElement {
+
     productIds = ['01t2w00000GeEpWAAV', '01t2w00000GeEpyAAF',
         '01t2w00000GeEpYAAV', '01t2w00000GeEpeAAF', '01t2w00000GeEq8AAF'];
+    @api productId;
+
+    addToQuote() {
+        addSelectedProducts({ productId: this.productId })
+            .then(result => {
+                console.log('Product Added to Quote');
+            })
+            .catch(error => {
+                console.error('Error Message:', error);
+            });
+    }
 }
--- force-app/main/default/lwc/opptyQuotePage/opptyQuotePage.js
+++ force-app/main/default/lwc/opptyQuotePage/opptyQuotePage.js
@@ -1,25 +1,25 @@
 import { LightningElement,api,wire } from 'lwc';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import OppQuote_CHANNEL from '@salesforce/messageChannel/OppQuote__c';
 import getOpportunity from '@salesforce/apex/opptyQuoteController.getOpportunitydata'
 import getQuote from '@salesforce/apex/opptyQuoteController.getQuotedata'
 
 export default class OpptyQuotePage extends LightningElement {
+  @wire(MessageContext)
+  messageContext;
     opportunityrecord;
     opportunityrecordId;
+    quoteRecordId;
     quoteRecord;
     OpportunityFields = ['Name','CloseDate','Description', 'StageName'];
     quoteFields = ['Name','SBQQ__LineItemsGrouped__c', 'SBQQ__LineItemsPrinted__c','SBQQ__Primary__c', 'IsDeleted'];
-    opportunityDisplay = true;
-    QuoteDisplay = false;
+    opportunityDisplay;
+    QuoteDisplay;
    
     @wire(getOpportunity) wiredOppty({data,error}){
         if (data) {
             this.opportunityrecord = data[0];
-            // this.OpportunityFields = [
-            //     { label: 'Name', fieldName: 'Name' },
-            //     { label: 'Close Date', fieldName: 'CloseDate'}
-            // ];
-            // this.Opportunity = data;
-           // this.opportunityrecordId = 
+           this.opportunityrecordId = this.opportunityrecord.Id;
             console.log(this.opportunityrecord.Id); 
             } else if (error) {
             console.log(error);
@@ -29,29 +29,38 @@ export default class OpptyQuotePage extends LightningElement {
     @wire(getQuote) wiredQuote({data,error}){
         if (data) {
             this.quoteRecord = data[0];
+            this.quoteRecordId = this.quoteRecord.Id;
             console.log(this.quoteRecord.Id); 
-            } else if (error) {
+            } 
+            else if (error) {
             console.log(error);
-            }
+        }
     }
-
-    //@wire(getQuote) wiredqoute;
-    // _oportunity = true;
-    // _quote = false;
-
-    // if(_oportunity)
-    // {
-    //     fields = ['Name', 'CloseDate', 'Description', 'StageName'];
-    //     objectApiName = 'Opportunity';
-    //     console.log(fields);
-    // }
-    // if(_quote)
-    // {
-    // fields = ['Name','SBQQ__LineItemsGrouped__c', 'SBQQ__LineItemsPrinted__c', 'SBQQ__Primary__c', 'IsDeleted'];
-    // objectApiName = 'SBQQ__Quote__c';
-    // console.log(fields);
-    // }
+    handleMessage(message) {
+      console.log(message);
+        if(message.OpptyOrQoute == 'Opportunity') {
+          //this.counter += message.constant;
+          this.opportunityDisplay = true;
+          this.QuoteDisplay = false;
+        }else if(message.OpptyOrQoute == 'Quote') {
+          //this.counter -= message.constant;
+          this.QuoteDisplay = true;
+          this.opportunityDisplay = false;
+        } else {
+          this.opportunityDisplay = false;
+          this.QuoteDisplay = false;
+        }
+      }
+    subscribeToMessageChannel() {
+        this.subscription = subscribe(
+          this.messageContext,
+          OppQuote_CHANNEL,
+          (message) => this.handleMessage(message)
+        );
+    }
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }    
     
-
 
 }
